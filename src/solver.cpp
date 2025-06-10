7473f2e5df77e64c2582f7285ace803b33cc14ed
dgordenin <dgordenin@gmail.com>
2025-06-10 20:34:56 +0300

    Changes after changing of solver.

--- src/solver.cpp
+++ src/solver.cpp
@@ -30,7 +30,7 @@ Solver::Solver(Document* _document) :
     break_loop(std::thread(&Solver::MessageLoop, this, std::ref(break_socket), std::ref(break_tasks), std::ref(break_next_circle)))
 #else
     service_config(logger),
-    service_context(&service_config, document->config),
+    service_context(&service_config, document->config.logs_path, document->config.log_console, document->config.log_file),
     session(&service_context, document->config),
     message_loop(std::thread(&Solver::MessageLoop, this, std::ref(socket), std::ref(tasks), std::ref(next_circle), std::ref(session))),
     break_loop(std::thread(&Solver::MessageLoop, this, std::ref(break_socket), std::ref(break_tasks), std::ref(break_next_circle), std::ref(session)))
